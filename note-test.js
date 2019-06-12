c584784697ce9725d04c3b36bc34922caba565cd
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-06-12 16:17:55 +0100

    user is now able to add a single note which will appear beneath the text box still need to include ability to add multiple notes in the same session still need to include text box wipe after input
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>
    Co-authored-by: Yemi <yemiadebayo60@gmail.com>"

--- note-test.js
+++ note-test.js
@@ -7,8 +7,24 @@ function testNoteListDefaultisEmpty(){
 
   function testAddNewNote(){
     var note = new Note();
-    note.newNote("Hello")
+    note.addNote("Hello")
     expect.include("Hello", note.list)
   };
 
   testAddNewNote();
+
+  function testFullNote(){
+    var note = new Note();
+    note.addNote("Lorem ipsum dolor sit amet");
+    expect.toEqual("Lorem ipsum dolor sit amet", note.showFullNote());
+  }
+
+  testFullNote();
+
+  function testShortNote(){
+    var note = new Note();
+    note.addNote("Lorem ipsum dolor sit amet");
+    expect.toEqual("Lorem ipsum dolor si...", note.showShortNote;
+  }
+
+  testShortNote();
--- note.js
+++ note.js
@@ -1,24 +1,34 @@
 (function(exports) {
     function Note() {
-        this.list = [];
+        this.list = new Array;
     };
 
-    Note.prototype.newNote = function(input) {
+    Note.prototype.addNote = function(input) {
         this.list.push(input)
     };
 
     Note.prototype.showShortNote = function() {
-        this.list.forEach(function(note) {
-            console.log(note.substring(0,20) + "...");
-        });
+        // this.list.forEach(function(note) {
+        //     return note.substring(0,20) + "...";
+        // });
+        var index
+        var array = this.list
+        for (index = 0; index < array.length; ++index) {
+            return array[index].substring(0,20) + "...";
+        };
     };
 
     Note.prototype.showFullNote = function() {
-        this.list.forEach(function(note) {
-            console.log(note);
-        });
+        // this.list.forEach(note => {
+        //     return note;
+        // });
+
+        var index
+        var array = this.list
+        for (index = 0; index < array.length; ++index) {
+            return array[index]
+        };
     };
 
     exports.Note = Note;
 })(this);
-
