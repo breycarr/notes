dd9ae59cb2b3fcea38d5cd43ceb5809b62e29695
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-06-14 13:00:18 +0100

    Refactored to extract methods for better readability, comments added to guide review
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>
    Co-authored-by: yem82 <yemiadebayo60@gmail.com>

--- controller.js
+++ controller.js
@@ -13,24 +13,35 @@ window.onload = function() {
     document.getElementById('input').value = "";
     output = noteLength(input);
     index = note.notePosition();
-    newLI = document.createElement('li');
-    newLI.setAttribute("id", index);
-    newLI.appendChild(document.createTextNode(output));
-    newLI.addEventListener("click", showFullNote);
+    newLI = listTagCreator(index, output);
     document.getElementById('notes area').appendChild(newLI);
   }
 
-  function showFullNote(newLI) {
-    let elementId = newLI.path[0].id;
-    var fullNote = note.getText(elementId);
-    document.write(fullNote);
-  }
-
+// Some of the functions below should be extracted to a model
+// However we're not sure which ones
   function noteLength(input) {
     if(input.length > 20) {
       return input.substring(0,20) + "...";
     } else {
       return input;
     }
+  }
+
+// This function is 5 lines for better readability and future changes
+  function listTagCreator(index, output) {
+    let element = document.createElement('li');
+    element.setAttribute("id", index);
+    element.appendChild(document.createTextNode(output));
+    element.addEventListener("click", showFullNote);
+    return element
+  }
+
+// Is there any better way we could display information?
+// Previous attempts used document.write()
+// but that limited the ability to return to the note list
+  function showFullNote(newLI) {
+    let elementId = newLI.path[0].id;
+    var fullNote = note.getText(elementId);
+    alert(fullNote);
   }
 }
