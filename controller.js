9cf7f0d25faac469f8cac736250387d86e36b49f
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-06-14 12:47:28 +0100

    Refactored logic to differentiate between different note lengths
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>
    Co-authored-by: yem82 <yemiadebayo60@gmail.com>

--- controller.js
+++ controller.js
@@ -10,19 +10,27 @@ window.onload = function() {
 
     input = document.getElementById('input').value;
     note.addNote(input);
-    document.getElementById('input').value = ""
-    output = input.substring(0,20)
-    index = note.notePosition()
+    document.getElementById('input').value = "";
+    output = noteLength(input);
+    index = note.notePosition();
     newLI = document.createElement('li');
-    newLI.setAttribute("id", index)
-    newLI.appendChild(document.createTextNode(output + "..."))
-    newLI.addEventListener("click", showFullNote)
-    document.getElementById('notes area').appendChild(newLI)
+    newLI.setAttribute("id", index);
+    newLI.appendChild(document.createTextNode(output));
+    newLI.addEventListener("click", showFullNote);
+    document.getElementById('notes area').appendChild(newLI);
   }
 
   function showFullNote(newLI) {
-    let elementId = newLI.path[0].id
-    var fullNote = note.getText(elementId)
-    document.write(fullNote)
+    let elementId = newLI.path[0].id;
+    var fullNote = note.getText(elementId);
+    document.write(fullNote);
+  }
+
+  function noteLength(input) {
+    if(input.length > 20) {
+      return input.substring(0,20) + "...";
+    } else {
+      return input;
+    }
   }
 }
--- note.js
+++ note.js
@@ -4,15 +4,15 @@
     };
 
     Note.prototype.addNote = function(input) {
-        this._list.push(input)
+        this._list.push(input);
     };
 
     Note.prototype.notePosition = function() {
-        return this._list.length - 1
+        return this._list.length - 1;
     };
 
     Note.prototype.getText = function(position){
-      return this._list[position]
+      return this._list[position];
     };
     exports.Note = Note;
 })(this);
