d0ae45b5d6ae8af5b5db7dfe4ddef7cf7bd00628
Yemi <yemiadebayo60@gmail.com>
2019-06-14 13:43:06 +0100

    Refactor code

--- note-test.js
+++ note-test.js
@@ -1,30 +1,22 @@
-function testNoteListDefaultisEmpty(){
+(function testNoteListDefaultisEmpty(){
     var note = new Note();
     expect.isLengthZero(note.list);
-  };
+  })();
 
-  testNoteListDefaultisEmpty();
-
-  function testAddNewNote(){
+(function testAddNewNote(){
     var note = new Note();
     note.addNote("Hello")
     expect.include("Hello", note.list)
-  };
+})();
 
-  testAddNewNote();
-
-  function testFullNote(){
+(function testFullNote(){
     var note = new Note();
     note.addNote("Lorem ipsum dolor sit amet");
     expect.toEqual("Lorem ipsum dolor sit amet", note.showFullNote());
-  }
+})();
 
-  testFullNote();
-
-  function testShortNote(){
+(function testShortNote(){
     var note = new Note();
     note.addNote("Lorem ipsum dolor sit amet");
-    expect.toEqual("Lorem ipsum dolor si...", note.showShortNote;
-  }
-
-  testShortNote();
+    expect.toEqual("Lorem ipsum dolor si...", note.showShortNote());
+})();
